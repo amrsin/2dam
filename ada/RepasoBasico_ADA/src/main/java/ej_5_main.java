056c62b32f2975480084263b8403f406cf4448e9
amrsin <amrsin@alu.iesserpis.org>
2020-09-18 18:40:11 +0200

    Realizado el ej5

--- ada/RepasoBasico_ADA/src/main/java/ej_5_main.java
+++ ada/RepasoBasico_ADA/src/main/java/ej_5_main.java
@@ -12,23 +12,37 @@ public class ej_5_main {
 
     public static void main(String[] args) {
 
-        int numero;
-
+        int num_random;
+        int num;
         int num_aleatorios[] = new int[15];
-
+        boolean encontrado = false;
         for (int i = 0; i < num_aleatorios.length; i++) {
 
-            numero = (int) (Math.random() * 90 + 1);
+            num_random = (int) (Math.random() * 90 + 1);
+            
 
             for (int j = 0; j < num_aleatorios.length; j++) {
-                
-                
-
+                    
+                if (num_random == num_aleatorios[j]) {
+                    encontrado = true;
+                    i--;
+                    break;
+                } 
             }
-
+            
+            if (!encontrado) {
+ 
+            num_aleatorios[i] = num_random;
+                
+            }
+           
+          encontrado = false;
         }
+        System.out.println("----------------");
+        System.out.println("Carton bingo");
+        for (int i = 0; i < num_aleatorios.length; i++) {
 
+            System.out.print( num_aleatorios[i] + "|");
+        }
     }
-
-} 
-
+}
