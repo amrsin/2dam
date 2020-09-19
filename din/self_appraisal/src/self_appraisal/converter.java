1d9a4f043bd0cca640a3654eddceb9c91816cf24
amrsin <amrsin@alu.iesserpis.org>
2020-09-19 14:08:20 +0200

    Finished self_appralsal activity

--- din/self_appraisal/src/self_appraisal/converter.java
+++ din/self_appraisal/src/self_appraisal/converter.java
@@ -1,55 +1,77 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package self_appraisal;
 
+package self_appraisal;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+
 /**
  *
  * @author amrsin
  */
-public class converter extends JFrame{
+public class converter extends JFrame {
 
-   
     JTextField textField1, textField2;
     JPanel panel = new JPanel();
-    
+
     public converter() {
-       
+
         setTitle("Converter");
-        setSize(400,300);
+        setSize(300, 200);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        panel.setLayout(new GridLayout(3,2));
-        
+        panel.setLayout(new GridLayout(3, 2));
+     
         JLabel lb_celsius = new JLabel(" Celsius Grades");
-        textField1 = new JTextField ("", 2);
+        textField1 = new JTextField("", 20);
+        textField1.setBorder(BorderFactory.createLineBorder(Color.red));
         textField1.addActionListener(new TextFieldListener());
         panel.add(lb_celsius);
-        textField1.setBorder (BorderFactory.createLineBorder (Color.red));
         panel.add(textField1);
+
         JLabel lb_kelvin = new JLabel(" Kelvin Grades");
+        textField2 = new JTextField("", 20);
+        textField2.setBorder(BorderFactory.createLineBorder(Color.red));
+        textField2.addActionListener(new TextFieldListener2());
         panel.add(lb_kelvin);
-        textField2 = new JTextField ("", 2);
-        textField2.setBorder (BorderFactory.createLineBorder (Color.red));
         panel.add(textField2);
+
+        JButton bt_clean = new JButton("Clean");
+        bt_clean.addActionListener(new ButtonListener());
+        panel.add(bt_clean);
         add(panel);
     }
-    
-      class TextFieldListener implements ActionListener {
+
+    class TextFieldListener implements ActionListener {
 
         public void actionPerformed(ActionEvent e) {
-// get the text typed in textfield1
-// and assign it to the other textfield.
+
             String text = textField1.getText();
-            float celcius = Float.parseFloat(text);
-            float tokelvin = (float) (273.15 + celcius);
-            text = Float.toString(tokelvin);
+            double celcius = Double.parseDouble(text);
+            Double tokelvin = 273.15 + celcius;
+            text = Double.toString(tokelvin);
             textField2.setText(text);
 
+        }
+    }
+
+    class TextFieldListener2 implements ActionListener {
+
+        public void actionPerformed(ActionEvent e) {
+
+            String text = textField2.getText();
+            double kelvin = Double.parseDouble(text);
+            double tocelcius = kelvin - 273.15;
+            text = Integer.toString((int) tocelcius);
+            textField1.setText(text);
+        }
+    }
+
+    class ButtonListener implements ActionListener {
+
+        public void actionPerformed(ActionEvent e) {
+            
+            textField1.setText("");
+            textField1.requestFocus(); 
+            textField2.setText("");
         }
     }
 }
