84dfde27da5cd13c0191b328544cb4a80a436674
amrsin <amrsin@alu.iesserpis.org>
2020-10-19 09:46:54 +0200

    actualizado

--- pmdm/prueba_CheckBox/app/src/main/java/com/example/prueba_checkbox/MainActivity.java
+++ pmdm/prueba_CheckBox/app/src/main/java/com/example/prueba_checkbox/MainActivity.java
@@ -2,23 +2,33 @@ package com.example.prueba_checkbox;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CheckBox chkbox_Cycling;
+    private CheckBox chkbox_Teaching;
+    private CheckBox chkbox_Blogging;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final CheckBox chkbox_Cycling = (CheckBox)findViewById(R.id.chkbox_Cycling);
-        final CheckBox chkbox_Teaching = (CheckBox)findViewById(R.id.chkbox_Teaching);
-        final CheckBox chkbox_Blogging = (CheckBox)findViewById(R.id.chkbox_Blogging);
+        chkbox_Cycling = (CheckBox)findViewById(R.id.chkbox_Cycling);
+        chkbox_Teaching = (CheckBox)findViewById(R.id.chkbox_Teaching);
+        chkbox_Blogging = (CheckBox)findViewById(R.id.chkbox_Blogging);
         Button btn_hobby = (Button)findViewById(R.id.btn_hobby);
+        Button btn_checbox_v2 = (Button)findViewById(R.id.btn_chk_v2);
+
         final TextView tv_hobby = (TextView)findViewById(R.id.tv_hobby);
 
         btn_hobby.setOnClickListener(new View.OnClickListener() {
@@ -42,6 +52,16 @@ public class MainActivity extends AppCompatActivity {
                     msg = msg + "Blogging ";
                 }
                 tv_hobby.setText(msg);
+            }
+        });
+
+        btn_checbox_v2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent_activity2 = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent_activity2);
+
             }
         });
 
