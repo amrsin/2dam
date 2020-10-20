88d470fa336aee6f24b1012985c951258c3a4722
amrsin <amrsin@alu.iesserpis.org>
2020-10-20 21:59:36 +0200

    updated

--- din/TestMySQL/src/testmysql/TestMySQL.java
+++ din/TestMySQL/src/testmysql/TestMySQL.java
@@ -5,6 +5,8 @@
  */
 package testmysql;
 
+import ui.frame;
+
 /**
  *
  * @author amrsin
@@ -16,6 +18,9 @@ public class TestMySQL {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        frame f = new frame();
+        f.setVisible(true);
     }
     
 }
