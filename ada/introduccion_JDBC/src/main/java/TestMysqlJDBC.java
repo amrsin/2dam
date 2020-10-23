eb2847cba64bb2f66357988ecf762d11b002463d
amrsin <amrsin@alu.iesserpis.org>
2020-10-23 20:29:53 +0200

    continuado con introduccion_JDBC

--- ada/introduccion_JDBC/src/main/java/TestMysqlJDBC.java
+++ ada/introduccion_JDBC/src/main/java/TestMysqlJDBC.java
@@ -1,11 +1,6 @@
 
 import java.util.*;
 import java.sql.*;
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /**
  *
@@ -13,20 +8,36 @@ import java.sql.*;
  */
 public class TestMysqlJDBC {
     
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) {
+        
+        String url = "jdbc:mysql://localhost:3306/test_ada?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+        String sql = "Select id_persona, nombre, apellidos, edad FROM persona";
         
         try {
-            
-            String url = "jdbc:mysql://localhost:3306/test_ada?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
-            Class.forName("com.mysql.oj-jdbc.Driver");
-            
+
+            //Class.forName("com.mysql.oj-jdbc.Driver");
             Connection conexion = DriverManager.getConnection(url, "root", "din123");
             
+            Statement instuccion = conexion.createStatement();
             
-        } catch (ClassNotFoundException ex) {
+            ResultSet rs = instuccion.executeQuery(sql);
+            
+            while (rs.next()) {                
+                
+                System.out.print("ID persona: " + rs.getInt("id_persona"));
+                System.out.print(", Nombre: " + rs.getString("nombre"));
+                System.out.print(", Apellidos: " + rs.getString("apellidos"));
+                System.out.print(", Edad: " + rs.getInt("edad") + "\n");
+                
+            }
+            
+            rs.close();
+            instuccion.close();
+            conexion.close();
+            
+        } catch (SQLException ex) {
             
             ex.printStackTrace(System.out);
         }
     }
-    
 }
