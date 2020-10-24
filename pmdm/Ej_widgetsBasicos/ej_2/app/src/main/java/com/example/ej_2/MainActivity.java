50398867756e2a7bb12b9ab1a568294c783548c6
amrsin <amrsin@alu.iesserpis.org>
2020-10-24 18:46:32 +0200

    acabado ej_2 ej_widgetsBasicos

---
+++ pmdm/Ej_widgetsBasicos/ej_2/app/src/main/java/com/example/ej_2/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.ej_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageButton img_btn_red;
+    private ImageButton img_btn_blue;
+    private TextView tv_color;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        img_btn_red = (ImageButton)findViewById(R.id.img_bt_red);
+        img_btn_blue = (ImageButton)findViewById(R.id.img_bt_blue);
+        tv_color = (TextView)findViewById(R.id.tv_color);
+
+        img_btn_red.setOnClickListener(new img_listener());
+        img_btn_blue.setOnClickListener(new img_listener());
+
+    }
+
+    class img_listener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View view) {
+
+            if (view == img_btn_red) {
+
+                tv_color.setBackgroundColor(Color.RED);
+
+            }else if(view == img_btn_blue) {
+
+                tv_color.setBackgroundColor(Color.BLUE);
+
+            }
+        }
+    }
+}
