82ef95a7640deb21cee3e47cd075994ea66b103d
amrsin <amrsin@alu.iesserpis.org>
2020-10-27 09:24:11 +0100

    acabado

--- ada/JDBC/Manejo_JDBC/src/main/java/datos/Conexion.java
+++ ada/JDBC/Manejo_JDBC/src/main/java/datos/Conexion.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package datos;
 
 import java.sql.*;
@@ -36,5 +32,4 @@ public class Conexion {
         con.close();
     }
     
-}   
-
+}
