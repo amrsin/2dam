9e27019b8d0b8ca20fac88dc8066e711f31232c2
amrsin <amrsin@alu.iesserpis.org>
2020-10-31 15:05:31 +0100

    updated

--- din/Practice_extra_ev1/src/practice_extra_ev1/Frame.java
+++ din/Practice_extra_ev1/src/practice_extra_ev1/Frame.java
@@ -3,6 +3,7 @@ package practice_extra_ev1;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import javax.swing.event.*;
 
 /**
  *
@@ -13,27 +14,30 @@ public class Frame extends JFrame {
     JPanel panel_general = new JPanel();
     JPanel panel_combo = new JPanel();
     String[] combo_names = {"None", "Blue", "Red", "Green", "Pink"};
-    Color[] combo_color = {panel_combo.getBackground(), Color.BLUE, Color.RED, Color.GREEN, Color.pink};
+    Color[] combo_color = {Color.BLACK, Color.BLUE, Color.RED, Color.GREEN, Color.pink};
     JComboBox combo;
     JPanel panel_checkbox_radio = new JPanel();
-    JLabel check_label;
+    JLabel combo_label, check_label, list_label;
     JCheckBox check_bold, check_italic;
     ButtonGroup bg = new ButtonGroup();
-    JRadioButton radio_24, radio_34;
+    JRadioButton radio_12, radio_24, radio_34;
+    JPanel panel_list = new JPanel();
+    JList list;
 
     public Frame() {
 
         setTitle("Practice");
-        setSize(700, 500);
+        setSize(600, 400);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
         panel_general.setLayout(new BorderLayout());
         panel_combo.setLayout(new GridLayout(1, 2));
         panel_checkbox_radio.setLayout(new GridLayout(1, 2));
+        panel_list.setLayout(new GridLayout(2, 0));
 
         add(panel_general);
 
-        JLabel combo_label = new JLabel(" Choose backgroud color: ");
+        combo_label = new JLabel(" Choose backgroud color: ");
         panel_combo.add(combo_label);
         combo = new JComboBox(combo_names);
         combo.addItemListener(new listener_combo());
@@ -41,7 +45,7 @@ public class Frame extends JFrame {
         panel_general.add(panel_combo, BorderLayout.NORTH);
 
         check_label = new JLabel(" text ");
-        check_label.setFont(new Font("serif", Font.PLAIN, 14));
+        check_label.setFont(new Font("serif", Font.PLAIN, 12));
 
         check_bold = new JCheckBox("Bold text");
         check_italic = new JCheckBox("Italic text");
@@ -50,18 +54,28 @@ public class Frame extends JFrame {
         panel_checkbox_radio.add(check_label);
         panel_checkbox_radio.add(check_bold);
         panel_checkbox_radio.add(check_italic);
-        
-        radio_24 = new JRadioButton("24");
-        radio_34 = new JRadioButton("34");
+
+        radio_12 = new JRadioButton("DEFAULT");
+        radio_24 = new JRadioButton("Size 24");
+        radio_34 = new JRadioButton("Size 34");
+        bg.add(radio_12);
         bg.add(radio_24);
         bg.add(radio_34);
+        radio_12.addItemListener(new listener_radio());
         radio_24.addItemListener(new listener_radio());
         radio_34.addItemListener(new listener_radio());
-
+        panel_checkbox_radio.add(radio_12);
         panel_checkbox_radio.add(radio_24);
         panel_checkbox_radio.add(radio_34);
-        
+
         panel_general.add(panel_checkbox_radio, BorderLayout.SOUTH);
+
+        list_label = new JLabel("List text");
+        list = new JList(combo_names);
+        list.addListSelectionListener(new listener_list());
+        panel_list.add(list_label);
+        panel_list.add(list);
+        panel_general.add(panel_list, BorderLayout.CENTER);
 
     }
 
@@ -72,7 +86,7 @@ public class Frame extends JFrame {
 
             int selected_item = combo.getSelectedIndex();
 
-            panel_general.setBackground(combo_color[selected_item]);
+            combo_label.setForeground(combo_color[selected_item]);
 
         }
     }
@@ -81,8 +95,8 @@ public class Frame extends JFrame {
 
         @Override
         public void itemStateChanged(ItemEvent e) {
-            
-              Font f = check_label.getFont();
+
+            Font f = check_label.getFont();
             int font_type = Font.PLAIN;
 
             if (check_bold.isSelected()) {
@@ -101,28 +115,43 @@ public class Frame extends JFrame {
 
         }
     }
-    
+
     class listener_radio implements ItemListener {
 
         @Override
         public void itemStateChanged(ItemEvent e) {
-            
+
             Font f = check_label.getFont();
-            
-            if (radio_24.isSelected()) {
-                
-                check_label.setFont(new Font("Serif", f.getStyle(), 24)); 
-                
+
+            if (radio_12.isSelected()) {
+
+                check_label.setFont(new Font("Serif", f.getStyle(), 12));
+
             }
-            
+
+            if (radio_24.isSelected()) {
+
+                check_label.setFont(new Font("Serif", f.getStyle(), 24));
+
+            }
+
             if (radio_34.isSelected()) {
-                
-                check_label.setFont(new Font("Serif", f.getStyle(), 34)); 
-                
+
+                check_label.setFont(new Font("Serif", f.getStyle(), 34));
+
             }
         }
-        
-        
-        
+    }
+
+    class listener_list implements ListSelectionListener {
+
+        @Override
+        public void valueChanged(ListSelectionEvent e) {
+
+            String selected_item = list.getSelectedValue().toString();
+
+            list_label.setText(selected_item);
+
+        }
     }
 }
