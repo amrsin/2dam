bce5f89db57566dbb781c92b8e784c1d015b875f
amrsin <amrsin@alu.iesserpis.org>
2020-11-03 09:26:04 +0100

    updated

--- pmdm/prueba_widgetsBasicos/RadioButton_1/app/src/main/java/com/example/radiobutton_1/MainActivity.java
+++ pmdm/prueba_widgetsBasicos/RadioButton_1/app/src/main/java/com/example/radiobutton_1/MainActivity.java
@@ -13,33 +13,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //var
+        final TextView lbl_radios = (TextView) findViewById(R.id.lbl_radios);
+        final RadioGroup radio_g = (RadioGroup) findViewById(R.id.group_radio);
 
-        final TextView lbl_radios = (TextView)findViewById(R.id.lbl_radios);
-        final RadioGroup radio_g = (RadioGroup)findViewById(R.id.group_radio);
-
-        radio_g.clearCheck();
+        //al seleccionar radio del grupo radio_g
         radio_g.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
                 String op = "";
+                //si es radio_1
+                if (group.getCheckedRadioButtonId() == R.id.radio_1) {
 
-                if (group.getCheckedRadioButtonId()==R.id.radio_1) {
-
-                    op = op + "OPCION 1 con ID: " + checkedId;
-
-                }
-
-                if (group.getCheckedRadioButtonId()==R.id.radio_2) {
-
-                    op = op + "OPCION 2 con ID: " + checkedId;
+                    op = "OPCION 1 con ID: " + checkedId; //id del radio_1
 
                 }
+                //si es radio_2
+                if (group.getCheckedRadioButtonId() == R.id.radio_2) {
 
-
+                    op = "OPCION 2 con ID: " + checkedId; //id del radio_2
+                }
+                //settext con valor de variable op
                 lbl_radios.setText(op);
             }
         });
-
-
     }
 }
