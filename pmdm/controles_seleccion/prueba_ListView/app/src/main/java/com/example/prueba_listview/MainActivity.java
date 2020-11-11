6d010ae5167fa638e17568d41dfed8e49ad7cc0a
amrsin <amrsin@alu.iesserpis.org>
2020-11-11 16:02:46 +0100

    updated

--- pmdm/controles_seleccion/prueba_ListView/app/src/main/java/com/example/prueba_listview/MainActivity.java
+++ pmdm/controles_seleccion/prueba_ListView/app/src/main/java/com/example/prueba_listview/MainActivity.java
@@ -11,7 +11,7 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    //var
     ListView lview;
     final static String semana[] = {"Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"};
 
@@ -19,13 +19,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //var
         String msg;
         lview = (ListView)findViewById(R.id.list1);
-
+        //ArrayAdapter en cual escogemos la plantilla ya creada de list "simple_list_item_1 en la cual añadimos el array de semana
         ArrayAdapter<String> miAdaptador = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, semana);
-        lview.setAdapter(miAdaptador);
+        lview.setAdapter(miAdaptador);//establecemos el adapter a la lview
 
+        //listerner lview  en cual indicara en toast el elemento seleciconado
         lview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -37,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
-
+    //toast del elemnto seleccionado
      public void showToast(String text) {
 
          Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
