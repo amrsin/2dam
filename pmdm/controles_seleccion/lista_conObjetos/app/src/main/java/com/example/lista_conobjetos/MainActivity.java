d91305c58b697025e79a8273cff0d2eb241429f9
amrsin <amrsin@alu.iesserpis.org>
2020-11-11 16:19:58 +0100

    updated

--- pmdm/controles_seleccion/lista_conObjetos/app/src/main/java/com/example/lista_conobjetos/MainActivity.java
+++ pmdm/controles_seleccion/lista_conObjetos/app/src/main/java/com/example/lista_conobjetos/MainActivity.java
@@ -18,7 +18,7 @@ import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
-
+    //datos de libros
     private Titular[] datos = new Titular[]{
             new Titular("Titulo1", "Subtitulo largo1", R.drawable.ic_launcher_foreground),
             new Titular("Titulo2", "Subtitulo largo2", R.drawable.ic_launcher_foreground),
@@ -30,21 +30,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        AdaptadorTitulares adaptador = new AdaptadorTitulares(this);
-        ListView list_titular = (ListView) findViewById(R.id.list_titular);
-        list_titular.setAdapter(adaptador);
-
+        AdaptadorTitulares adaptador = new AdaptadorTitulares(this); //llamamos a la clase
+        ListView list_titular = (ListView) findViewById(R.id.list_titular); //identificamos la lista con id
+        list_titular.setAdapter(adaptador);//establecemos adapter a la lista
+        //listener que lanza un toast con datos libro
         list_titular.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
-                String msg = "Titulo " + datos[position].getTitulo() + " Subtitulo: " + datos[position].getSubtitulo();
+                String msg = "Titulo: " + datos[position].getTitulo() + ", Subtitulo: " + datos[position].getSubtitulo();
                 Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
             }
         });
     }
 
-
+    //clase para rellenar datos a la list
     class AdaptadorTitulares extends ArrayAdapter {
 
         Activity context;
@@ -59,16 +59,15 @@ public class MainActivity extends AppCompatActivity {
 
             LayoutInflater inflater = context.getLayoutInflater();
             View item = inflater.inflate(R.layout.list_layout, null);
-
+            //asignamos el titulo
             TextView tv_titulo = (TextView) item.findViewById(R.id.tv_Titulo);
             tv_titulo.setText(datos[i].getTitulo());
-
+            //asignamos el subtitulo
             TextView tv_subtitulo = (TextView) item.findViewById(R.id.tv_subtitulo);
             tv_subtitulo.setText(datos[i].getSubtitulo());
-
+            //asignamos la imagen
             ImageView img = (ImageView) item.findViewById(R.id.img);
             img.setBackground(getDrawable(datos[i].getImg()));
-
             return (item);
         }
     }
--- pmdm/controles_seleccion/lista_conObjetos/app/src/main/java/com/example/lista_conobjetos/Titular.java
+++ pmdm/controles_seleccion/lista_conObjetos/app/src/main/java/com/example/lista_conobjetos/Titular.java
@@ -1,18 +1,18 @@
 package com.example.lista_conobjetos;
 
 public class Titular {
-
+    //var
     private String titulo;
     private String subtitulo;
     private  int img;
-
+    //constructor con todos los datos
     public Titular(String titulo, String subtitulo, int img){
 
         this.titulo = titulo;
         this.subtitulo = subtitulo;
         this.img = img;
     }
-
+    //getters
     public String getTitulo() {
         return titulo;
     }
