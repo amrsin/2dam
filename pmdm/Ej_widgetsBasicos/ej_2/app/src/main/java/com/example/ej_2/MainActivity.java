fac210039eb4cf58429a366279b6c24488ee5aaf
amrsin <amrsin@alu.iesserpis.org>
2020-11-12 09:29:14 +0100

    updated

--- pmdm/Ej_widgetsBasicos/ej_2/app/src/main/java/com/example/ej_2/MainActivity.java
+++ pmdm/Ej_widgetsBasicos/ej_2/app/src/main/java/com/example/ej_2/MainActivity.java
@@ -17,21 +17,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //identificamos los elementos con el correspondiente ed
         img_btn_red = (ImageButton)findViewById(R.id.img_bt_red);
         img_btn_blue = (ImageButton)findViewById(R.id.img_bt_blue);
         tv_color = (TextView)findViewById(R.id.tv_color);
-
+        //añadimos listener a los dos img_btn creando neeva clase img_listener
         img_btn_red.setOnClickListener(new img_listener());
         img_btn_blue.setOnClickListener(new img_listener());
 
     }
-
+    //clase img_listener
     class img_listener implements View.OnClickListener {
 
         @Override
         public void onClick(View view) {
-
+            //dependiendo quien llame a la clase asignara el color
             if (view == img_btn_red) {
 
                 tv_color.setBackgroundColor(Color.RED);
