2b8f9188edfe63793258dfd2062196232a9b8df9
amrsin <amrsin@alu.iesserpis.org>
2020-11-12 09:16:11 +0100

    updated

--- pmdm/Ej_widgetsBasicos/ej_1/app/src/main/java/com/example/ej_1/MainActivity.java
+++ pmdm/Ej_widgetsBasicos/ej_1/app/src/main/java/com/example/ej_1/MainActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
-
+    //var
     private ImageButton imgBtn_linearLayout;
     private ImageButton imgBtn_Table_Layout;
     private ImageButton imgBtn_ConstraintLayout;
@@ -18,25 +18,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //identificamos los elementos con sus correspondiente id
         imgBtn_linearLayout = (ImageButton)findViewById(R.id.imgBtn_linearLayout);
         imgBtn_Table_Layout = (ImageButton)findViewById(R.id.imgBtn_TableLayout);
         imgBtn_ConstraintLayout = (ImageButton)findViewById(R.id.imgBtn_ConstraintLayout);
         imgBtn_GridLayout = (ImageButton)findViewById(R.id.imgBtn_GridLayout);
-
+        //añadimos listener a cada imgbtn llamando a clase img_btn_listener
         imgBtn_linearLayout.setOnClickListener(new img_btn_Listener());
         imgBtn_Table_Layout.setOnClickListener(new img_btn_Listener());
         imgBtn_ConstraintLayout.setOnClickListener(new img_btn_Listener());
         imgBtn_GridLayout.setOnClickListener(new img_btn_Listener());
     }
-
+    //listener de imgbTN
     class img_btn_Listener implements View.OnClickListener {
 
         @Override
         public void onClick(View view) {
             int activity = 0;
             Bundle bundle = new Bundle();
-
+            //Segun quien llame a esta clase se asigna la variable activity con su identificador de activity y lo panemos en el bundel
             if (view == imgBtn_linearLayout ) {
 
                 activity = R.layout.linearlayout;
@@ -65,14 +65,11 @@ public class MainActivity extends AppCompatActivity {
                 bundle.putInt("activity", activity);
 
             }
-
+            //intent para segundoActivity y añadimos bundel al intent
             Intent intent = new Intent(MainActivity.this, SegundoActivity.class);
             intent.putExtras(bundle);
-            startActivity(intent);
-
+            startActivity(intent);// iniciamos el segundoActivity
 
         }
     }
-
-
 }
--- pmdm/Ej_widgetsBasicos/ej_1/app/src/main/java/com/example/ej_1/SegundoActivity.java
+++ pmdm/Ej_widgetsBasicos/ej_1/app/src/main/java/com/example/ej_1/SegundoActivity.java
@@ -15,34 +15,34 @@ public class SegundoActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(activity());
-
+        setContentView(activity());//le asignamos el layout  recogiendo del bundle el int de activity correspondiente
+        //identificamos elementos segun su id
         final CheckBox chk_prueba = (CheckBox)findViewById(R.id.chk_prueba);
         Button btn_vuelveMain = (Button)findViewById(R.id.btn_vuelveMain);
-
+        //listener para el btn
         btn_vuelveMain.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                //intent para volver a MainActivity
                 Intent intent = new Intent(SegundoActivity.this, MainActivity.class);
-                startActivity(intent);
+                startActivity(intent);//inicamos MainActivity
             }
         });
-
+        //listener para el chk
         chk_prueba.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                //si esta checked entonces lanza toast de que haz marcado el checkbox
                 if (chk_prueba.isChecked()) {
 
                     Toast.makeText(SegundoActivity.this, "Haz marcado el checkBox", Toast.LENGTH_SHORT).show();
-                }else {
+                }else {//sino toast de que haz desmarcado el checkbox
                     Toast.makeText(SegundoActivity.this, "Haz desmarcado el checkbox", Toast.LENGTH_SHORT).show();
                 }
             }
         });
     }
-
+    //metodo para recoger el bundle de MainActivity con el int de layout a asignar
     public int activity() {
 
         Bundle bunde_recoge = getIntent().getExtras();
