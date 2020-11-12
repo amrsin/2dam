71acb4568a4bc7820b0f03c4c6b045668953bc6e
amrsin <amrsin@alu.iesserpis.org>
2020-11-12 18:30:19 +0100

    Finished login frame

---
+++ din/auto_dealer/src/DAO/adminDAO.java
@@ -0,0 +1,56 @@
+package DAO;
+
+/**
+ *
+ * @author singh
+ */
+import java.sql.*;
+import javax.swing.*;
+
+public class adminDAO {
+
+    public boolean verify_login(JTextField txt_username, JPasswordField password) throws Exception {
+
+        Boolean exist = false;
+        Connection con = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+
+        if (!txt_username.getText().isEmpty() && !password.getText().isEmpty()) {
+
+            try {
+
+                con = Connection_BD.OpenConnection();
+                stmt = con.prepareStatement("SELECT * FROM admin where username=? AND password=?");
+                stmt.setString(1, txt_username.getText());
+                stmt.setString(2, password.getText());
+                rs = stmt.executeQuery();
+
+                if (rs.next()) {
+
+                    exist = true;
+                    JOptionPane.showMessageDialog(null, "Username and Password matched");
+                } else {
+                    JOptionPane.showMessageDialog(null, "Username and Password are not correct");
+                }
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null, ex, "ERROR", JOptionPane.ERROR_MESSAGE);
+            } finally {
+                rs.close();;
+                stmt.close();
+                Connection_BD.CloseConnection(con);
+            }
+        } else {
+            
+            if (txt_username.getText().isEmpty()) {
+                JOptionPane.showMessageDialog(null, "Username field is empty", "WARNING", JOptionPane.WARNING_MESSAGE);
+
+            }
+            if (password.getText().isEmpty()) {
+                JOptionPane.showMessageDialog(null, "Password field is empty","WARNING", JOptionPane.WARNING_MESSAGE);
+
+            }
+        }
+        return exist;
+    }
+}
---
+++ din/auto_dealer/src/View/Frame_autodealer.java
@@ -0,0 +1,19 @@
+
+package View;
+
+import javax.swing.*;
+
+/**
+ *
+ * @author singh
+ */
+public class Frame_autodealer extends JFrame{
+
+    public Frame_autodealer() {
+        
+        setSize(600,300);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+    }
+}
