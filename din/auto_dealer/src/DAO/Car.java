d15cddf64e0654b857f2cb9f29e2efdcc3491356
amrsin <amrsin@alu.iesserpis.org>
2020-11-13 15:36:53 +0100

    updated

---
+++ din/auto_dealer/src/DAO/Car.java
@@ -0,0 +1,84 @@
+
+package DAO;
+
+/**
+ *
+ * @author singh
+ */
+public class Car {
+    
+    private String License_plate;
+    private String Brand;
+    private String Model;
+    private String Year;
+    private String Color;
+    private String Kilometres;
+    private String Fuel;
+    private String Doors;
+    private String Gear_change;
+    private String Seats;
+    private String price;
+
+    public Car() {
+    }
+
+    public Car(String License_plate, String Brand, String Model, String Year, String Color, String Kilometres, String Fuel,String Doors, String Gear_change, String Seats, String price) {
+        this.License_plate = License_plate;
+        this.Brand = Brand;
+        this.Model = Model;
+        this.Year = Year;
+        this.Color = Color;
+        this.Kilometres = Kilometres;
+        this.Fuel = Fuel;
+        this.Doors = Doors;
+        this.Gear_change = Gear_change;
+        this.Seats = Seats;
+        this.price = price;
+    }
+
+    public String getLicense_plate() {
+        return License_plate;
+    }
+
+    public String getBrand() {
+        return Brand;
+    }
+
+    public String getModel() {
+        return Model;
+    }
+
+    public String getYear() {
+        return Year;
+    }
+
+    public String getColor() {
+        return Color;
+    }
+
+    public String getKilometres() {
+        return Kilometres;
+    }
+
+    public String getFuel() {
+        return Fuel;
+    }
+    
+     public String getDoors() {
+        return Doors;
+    }
+
+
+    public String getGear_change() {
+        return Gear_change;
+    }
+
+    public String getSeats() {
+        return Seats;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+    
+}
--- din/auto_dealer/src/View/Frame_autodealer.java
+++ din/auto_dealer/src/View/Frame_autodealer.java
@@ -1,7 +1,12 @@
 
 package View;
 
+import DAO.CarDao;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 
 /**
  *
@@ -9,11 +14,48 @@ import javax.swing.*;
  */
 public class Frame_autodealer extends JFrame{
 
+    JTable table = new JTable();
+    DefaultTableModel model = new DefaultTableModel();
+    String [] headers = {"License plate", "Brand", "Model", "Year", "Color", "Kilometres", 
+                        "Fuel", "Doors", "Gear Change", "Seats", "Price"};
+    JButton btn_new, btn_delete, btn_update, btn_refresh;
+    JPanel panel_general= new JPanel();
+    JPanel panel_btn = new JPanel();
     public Frame_autodealer() {
         
-        setSize(600,300);
+        panel_general.setLayout(new BorderLayout());
+        panel_btn.setLayout(new FlowLayout());
+        setTitle("Auto Dealer");
+        setSize(1000,500);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        
+        model.setColumnIdentifiers(headers);
+        showTable();
+        table.setModel(model);
+        JScrollPane jScrollPane1 = new JScrollPane(table);
+        jScrollPane1.getViewport().setBackground(Color.WHITE);
 
+        panel_general.add(jScrollPane1, BorderLayout.NORTH);
+        
+        btn_new = new JButton("NEW");
+        btn_delete = new JButton("DELETE");
+        btn_update = new JButton("UPDATE");
+        btn_refresh = new JButton("REFRESH");
+        panel_btn.add(btn_new);
+        panel_btn.add(btn_delete);
+        panel_btn.add(btn_update);
+        panel_btn.add(btn_refresh);
+        panel_btn.setBackground(Color.WHITE);
+        panel_general.add(panel_btn, BorderLayout.CENTER);
+        
+        
+        add(panel_general);
     }
+    
+  public void showTable() {
+      
+      CarDao c = new CarDao();
+      model = c.fillTable(model);
+  }
 }
