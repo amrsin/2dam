55fa1a08cadd9d00695c7e5bf1790d624cb40bf7
amrsin <amrsin@alu.iesserpis.org>
2020-11-13 17:23:12 +0100

    updated

--- ada/JDBC/Practica_2_supercomprin/src/main/java/datos/Conexion.java
+++ ada/JDBC/Practica_2_supercomprin/src/main/java/datos/Conexion.java
@@ -9,7 +9,7 @@ import java.sql.*;
 public class Conexion {
     
     //datos necesarios para poder conectar a la bd
-    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/test_ada?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/supercomprin?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
     private static final String JDBC_USER = "root";
     private static final String JDBC_PASSWORD= "din123";
   
