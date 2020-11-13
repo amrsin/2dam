ed8adbbbd94003c47e6c9c3213501bc317f16355
amrsin <amrsin@alu.iesserpis.org>
2020-11-13 18:39:17 +0100

    updated

--- din/auto_dealer/src/View/Frame_autodealer.java
+++ din/auto_dealer/src/View/Frame_autodealer.java
@@ -1,43 +1,56 @@
-
 package View;
 
 import DAO.CarDao;
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.FlowLayout;
+import java.awt.*;
 import javax.swing.*;
+import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
 
 /**
  *
  * @author singh
  */
-public class Frame_autodealer extends JFrame{
+public class Frame_autodealer extends JFrame {
 
     JTable table = new JTable();
     DefaultTableModel model = new DefaultTableModel();
-    String [] headers = {"License plate", "Brand", "Model", "Year", "Color", "Kilometres", 
-                        "Fuel", "Doors", "Gear Change", "Seats", "Price"};
+    String[] headers = {"License plate", "Brand", "Model", "Year", "Color", "Kilometres",
+        "Fuel", "Doors", "Gear Change", "Seats", "Price"};
     JButton btn_new, btn_delete, btn_update, btn_refresh;
-    JPanel panel_general= new JPanel();
+    JPanel panel_general = new JPanel();
     JPanel panel_btn = new JPanel();
+
     public Frame_autodealer() {
-        
+
         panel_general.setLayout(new BorderLayout());
         panel_btn.setLayout(new FlowLayout());
+
         setTitle("Auto Dealer");
-        setSize(1000,500);
+        setSize(1000, 500);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        
+
         model.setColumnIdentifiers(headers);
+        
         showTable();
         table.setModel(model);
+        table.setRowHeight(30);
+
+        
+        DefaultTableCellRenderer tcr = new DefaultTableCellRenderer();
+        tcr.setHorizontalAlignment(SwingConstants.CENTER);
+        
+
+        for (int i = 0; i < model.getColumnCount(); i++) {
+
+            table.getColumnModel().getColumn(i).setCellRenderer(tcr);
+
+        }
+        
         JScrollPane jScrollPane1 = new JScrollPane(table);
         jScrollPane1.getViewport().setBackground(Color.WHITE);
+        panel_general.add(jScrollPane1, BorderLayout.CENTER);
 
-        panel_general.add(jScrollPane1, BorderLayout.NORTH);
-        
         btn_new = new JButton("NEW");
         btn_delete = new JButton("DELETE");
         btn_update = new JButton("UPDATE");
@@ -47,15 +60,14 @@ public class Frame_autodealer extends JFrame{
         panel_btn.add(btn_update);
         panel_btn.add(btn_refresh);
         panel_btn.setBackground(Color.WHITE);
-        panel_general.add(panel_btn, BorderLayout.CENTER);
-        
-        
+        panel_general.add(panel_btn, BorderLayout.SOUTH);
+
         add(panel_general);
     }
-    
-  public void showTable() {
-      
-      CarDao c = new CarDao();
-      model = c.fillTable(model);
-  }
+
+    public void showTable() {
+
+        CarDao c = new CarDao();
+        model = c.fillTable(model);
+    }
 }
