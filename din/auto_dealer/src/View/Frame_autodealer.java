1881863abe3bcaae809895279ff7839b9dd109cb
amrsin <amrsin@alu.iesserpis.org>
2020-11-15 10:31:50 +0100

    updated

--- din/auto_dealer/src/View/Frame_autodealer.java
+++ din/auto_dealer/src/View/Frame_autodealer.java
@@ -58,11 +58,11 @@ public class Frame_autodealer extends JFrame {
         btn_update = new JButton("UPDATE");
         btn_refresh = new JButton("REFRESH");
 
+        btn_new.addActionListener(new btn_listener());
         btn_delete.addActionListener(new btn_listener());
-        
+        btn_update.addActionListener(new btn_listener());
         btn_refresh.addActionListener(new btn_listener());
 
-       
         panel_btn.add(btn_new);
         panel_btn.add(btn_delete);
         panel_btn.add(btn_update);
@@ -78,14 +78,42 @@ public class Frame_autodealer extends JFrame {
         model = cardao.fillTable(model);
     }
 
-
     class btn_listener implements ActionListener {
 
         @Override
         public void actionPerformed(ActionEvent e) {
-
+            
+            
             Object source = e.getSource();
 
+            if (source == btn_new) {
+
+                String license_plate = JOptionPane.showInputDialog(null, "Enter License plate");
+                String Brand = JOptionPane.showInputDialog(null, "Enter Brand");
+                String Model = JOptionPane.showInputDialog(null, "Enter Model");
+                String Year = JOptionPane.showInputDialog(null, "Enter Year");
+                String Color = JOptionPane.showInputDialog(null, "Enter Color");
+                String Kilometres = JOptionPane.showInputDialog(null, "Enter Kilometres");
+                String Fuel = JOptionPane.showInputDialog(null, "Enter Fuel");
+                String Doors = JOptionPane.showInputDialog(null, "Enter Doors");
+                String Gear_change = JOptionPane.showInputDialog(null, "Enter Gear change");
+                String Seats = JOptionPane.showInputDialog(null, "Enter Seats");
+                String Price = JOptionPane.showInputDialog(null, "Enter Price");
+
+                if (license_plate.equals("") || Brand.equals("") || Model.equals("") || Year.equals("")
+                        || Color.equals("") || Kilometres.equals("") || Fuel.equals("") || Doors.equals("")
+                        || Gear_change.equals("") || Seats.equals("") || Price.equals("")) {
+                    
+                    JOptionPane.showMessageDialog(null, "Nothing will be added, you must enter all the data ", "WARNING", JOptionPane.WARNING_MESSAGE);
+                } else {
+
+                    Object rowData[] = {license_plate, Brand, Model, Year, Color, Kilometres, Fuel, Doors, Gear_change, Seats, Price};
+                    Car c = new Car(license_plate, Brand, Model, Year, Color, Kilometres, Fuel, Doors, Gear_change, Seats, Price);
+                    model.addRow(rowData);
+
+                }
+
+            }
             if (source == btn_delete) {
 
                 if (table.getSelectedRowCount() == 1) {
@@ -108,34 +136,16 @@ public class Frame_autodealer extends JFrame {
                     }
                 }
             }
-            
+
             if (source == btn_update) {
-                
+
                 if (table.getSelectedRowCount() == 1) {
-                    
-                    String license_plate = table.getValueAt(table.getSelectedRow(), 0).toString();
-                    String Brand = table.getValueAt(table.getSelectedRow(), 0).toString();
-                    String Model = table.getValueAt(table.getSelectedRow(), 1).toString();
-                    String Year = table.getValueAt(table.getSelectedRow(), 2).toString();
-                    String Color = table.getValueAt(table.getSelectedRow(), 3).toString();
-                    String Kilometres = table.getValueAt(table.getSelectedRow(), 4).toString();
-                    String Fuel = table.getValueAt(table.getSelectedRow(), 5).toString();
-                    String Doors = table.getValueAt(table.getSelectedRow(), 6).toString();
-                    String Gear_change = table.getValueAt(table.getSelectedRow(), 7).toString();
-                    String Seats = table.getValueAt(table.getSelectedRow(), 8).toString();
-                    String Price = table.getValueAt(table.getSelectedRow(), 9).toString();
-                    
-                    
-                    
-                    
-                    
-                    
+
                 }
-                
             }
-            
+
             if (source == btn_refresh) {
-                
+
                 model.setRowCount(0);
                 model = cardao.fillTable(model);
             }
