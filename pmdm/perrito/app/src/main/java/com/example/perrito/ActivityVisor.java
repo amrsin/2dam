05c44b2e20399fb8b8472fe847ac4a4b5425def7
amrsin <amrsin@alu.iesserpis.org>
2020-11-16 10:37:09 +0100

    updated

---
+++ pmdm/perrito/app/src/main/java/com/example/perrito/ActivityVisor.java
@@ -0,0 +1,14 @@
+package com.example.perrito;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityVisor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_visor);
+    }
+}
---
+++ pmdm/perrito/app/src/main/java/com/example/perrito/MainActivity.java
@@ -0,0 +1,100 @@
+package com.example.perrito;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    public final static int OPINION_REQUEST_CODE = 1;
+    public final static String EXTRA_NOMBRE = "com.example.perrito.NOMBRE";
+    private TextView tv_autores;
+    private Button btn_mascota;
+    private TextView tv_opnion;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        //identificamos elementos con sus correspondiente id y agregamos OnClickListener
+        tv_autores = (TextView) findViewById(R.id.tv_autores);
+        tv_autores.setOnClickListener(new click_listener());
+        btn_mascota = (Button) findViewById(R.id.btn_mascota);
+        btn_mascota.setOnClickListener(new click_listener());
+        tv_opnion = (TextView) findViewById(R.id.tv_opinion);
+
+
+    }
+
+    //class listener para al hacer click
+    class click_listener implements View.OnClickListener {
+
+
+        @Override
+        public void onClick(View v) {
+
+            if (v == tv_autores) {
+
+                navegador();//llamamos al metodo navegador
+
+
+            }
+
+            if (v == btn_mascota) {
+
+                //Iniciando la actividad Visor
+                Intent intent = new Intent(MainActivity.this, ActivityVisor.class);
+                //Adhesion de nuestra cadena
+                intent.putExtra(EXTRA_NOMBRE, "pet1.jpg");
+                //Inicio de la actividad esperando un resultado
+                startActivityForResult(intent,OPINION_REQUEST_CODE);
+            }
+        }
+
+        public void navegador() {
+
+            Uri webpage = Uri.parse("https://www.iesserpis.org/");//pagina web donde quemos ir
+            Intent webIntent = new Intent(Intent.ACTION_VIEW, webpage);//intente oara ir a la pagina
+
+            // Verificar si hay aplicaciones disponibles
+            PackageManager packageManager = getPackageManager();
+            List activities = packageManager.queryIntentActivities(webIntent, 0);
+            boolean isIntentSafe = activities.size() > 0;
+
+            // Si hay, entonces ejecutamos la actividad
+            if (isIntentSafe) {
+                Toast.makeText(MainActivity.this, "Iniciando navegador", Toast.LENGTH_SHORT).show();
+                startActivity(webIntent);
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+        if (requestCode == OPINION_REQUEST_CODE) {
+
+            if (resultCode == RESULT_OK) {
+
+                String result=data.getStringExtra("opinion");
+
+                tv_opnion.setText("Tu opinion fué "+result);
+
+            }
+        }
+
+    }
+}
