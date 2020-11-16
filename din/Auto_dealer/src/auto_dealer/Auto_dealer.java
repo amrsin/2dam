3fd6f27bad6544f1e5cc7d4d9da9e60cc32350bf
amrsin <amrsin@alu.iesserpis.org>
2020-11-16 12:38:37 +0100

    updated

---
+++ din/Auto_dealer/src/auto_dealer/Auto_dealer.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package auto_dealer;
+
+import View.Frame_login;
+
+/**
+ *
+ * @author amrsin
+ */
+public class Auto_dealer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Frame_login f =  new Frame_login();
+        f.setVisible(true);
+    }
+    
+}
