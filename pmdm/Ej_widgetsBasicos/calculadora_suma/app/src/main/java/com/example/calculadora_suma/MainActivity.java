ff7dfa8ab8d33a69c1378bd1e753fd9e8260a2ab
amrsin <amrsin@alu.iesserpis.org>
2020-11-17 20:41:36 +0100

    updated

--- pmdm/Ej_widgetsBasicos/calculadora_suma/app/src/main/java/com/example/calculadora_suma/MainActivity.java
+++ pmdm/Ej_widgetsBasicos/calculadora_suma/app/src/main/java/com/example/calculadora_suma/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,25 +18,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //identificar elementos con su correspondiente id
         edit_t1= (EditText) findViewById(R.id.txt_num1);
         edit_t2 = (EditText) findViewById(R.id.txt_num2);
         text_v = (TextView) findViewById(R.id.resultado);
         bt_suma = (Button) findViewById(R.id.button_suma);
-
+        //listener suma
         bt_suma.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                int num_1;
-                int num_2;
-                int suma;
-                num_1 = Integer.parseInt(edit_t1.getText().toString());
-                num_2 = Integer.parseInt(edit_t2.getText().toString());
-                suma = num_1 + num_2;
-                text_v.setText(String.valueOf(num_1 + " + " + num_2 + " = " +  suma));
+                try {
+                    int num_1;
+                    int num_2;
+                    int suma;
+                    num_1 = Integer.parseInt(edit_t1.getText().toString());
+                    num_2 = Integer.parseInt(edit_t2.getText().toString());
+                    suma = num_1 + num_2;
+                    text_v.setText(String.valueOf(num_1 + " + " + num_2 + " = " +  suma));
+                }catch (Exception e) {
+                    Toast.makeText(MainActivity.this, "Introduzca dos numeros enteros", Toast.LENGTH_SHORT).show();
+                }
             }
         });
-
     }
 }
