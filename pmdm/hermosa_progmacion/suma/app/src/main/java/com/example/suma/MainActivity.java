b977cb703bd3fcec2ee4b52e38522a9fafc85cfb
amrsin <amrsin@alu.iesserpis.org>
2020-11-17 20:50:18 +0100

    updated

--- pmdm/hermosa_progmacion/suma/app/src/main/java/com/example/suma/MainActivity.java
+++ pmdm/hermosa_progmacion/suma/app/src/main/java/com/example/suma/MainActivity.java
@@ -9,8 +9,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-
+    //var elementos
     private EditText et_num_1;
     private EditText et_num_2;
     private TextView tv_result;
@@ -21,17 +20,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        //identificando cada elemento con su id
         et_num_1 = (EditText) findViewById(R.id.num_1);
         et_num_2 = (EditText) findViewById(R.id.num_2);
         tv_result = (TextView)findViewById(R.id.tv_result);
         btn_suma = (Button) findViewById(R.id.btn_suma);
-
+        //añadiendo listener al btn_suma
         btn_suma.setOnClickListener(new btn_listener());
 
     }
 
-
+    //clase para btn_suma
     class btn_listener implements View.OnClickListener {
 
         int num_1;
@@ -41,10 +40,10 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onClick(View v) {
 
-            suma();
+            suma();//llamando al metodo suma
 
         }
-
+        //metodo para realizar la suma
         public void suma() {
 
             try {
@@ -59,8 +58,6 @@ public class MainActivity extends AppCompatActivity {
 
                 tv_result.setText("Introduzca numeros");
             }
-
-
         }
     }
 }
