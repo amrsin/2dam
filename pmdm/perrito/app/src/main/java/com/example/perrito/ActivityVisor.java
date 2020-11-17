2d6baf95a23a2116e3e87dd98d2ebb416e73c738
amrsin <amrsin@alu.iesserpis.org>
2020-11-17 20:30:19 +0100

    updated

--- pmdm/perrito/app/src/main/java/com/example/perrito/ActivityVisor.java
+++ pmdm/perrito/app/src/main/java/com/example/perrito/ActivityVisor.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ActivityVisor extends AppCompatActivity {
 
@@ -33,30 +34,36 @@ public class ActivityVisor extends AppCompatActivity {
         //Añadiendo escucha al botón send_button
         btn_send.setOnClickListener(new btn_listener());
 
-
     }
     //Listener para btn_send
     class btn_listener implements View.OnClickListener{
 
         @Override
         public void onClick(View view) {
-
+            String opinion = "";
             RadioButton currentRadio = (RadioButton) findViewById(radioGroup
                     .getCheckedRadioButtonId());
 
             //Obtener la cadena del radiobutton
-            String opinion = currentRadio.getText().toString();
+            try {
+                opinion = currentRadio.getText().toString();
+            }catch (Exception e) {
+                Toast.makeText(ActivityVisor.this, "Falta opinion, selecciona algun RadioButton porfavor", Toast.LENGTH_SHORT).show();
+            }
 
             Intent intent_vuelve = new Intent();//creamos intent
             Bundle bundle_vuelve = new Bundle();//craemos bundle
-            String msg = opinion;//msg con saludo para devolver al lbl MainActivity
-            bundle_vuelve.putString("opnion", msg);//ponemos en bundle el msg
-            intent_vuelve.putExtras(bundle_vuelve);//ponemos en intent el bundle
+            if (!opinion.equals("")) {
 
-            //Devolver por el canal de forma exitosa el mensaje del intent
-            setResult(RESULT_OK, intent_vuelve);
-            //Terminar la actividad
-            finish();
+                bundle_vuelve.putString("opnion", opinion);//ponemos en bundle var opnion
+                intent_vuelve.putExtras(bundle_vuelve);//ponemos en intent el bundle
+                //Devolver por el canal de forma exitosa el mensaje del intent
+                setResult(RESULT_OK, intent_vuelve);
+                //Terminar la actividad
+                finish();
+
+            }
+
         }
     }
 }
