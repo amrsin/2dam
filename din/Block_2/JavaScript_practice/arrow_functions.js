dd2967e0fcc5f75fc403797c3067da108c7fafa0
amrsin <amrsin@alu.iesserpis.org>
2020-11-30 13:35:53 +0100

    updated

---
+++ din/Block_2/JavaScript_practice/arrow_functions.js
@@ -0,0 +1,39 @@
+let data = [
+    {name: "Nacho", telephone: "966112233", age: 40},
+    {name: "Ana", telephone: "911223344", age: 35},
+    {name: "Mario", telephone: "611998877", age: 15},
+    {name: "Laura", telephone: "633663366", age: 17}
+    ];
+    
+    let newPerson = {name: "Juan", phone: "965661564", age: 60};
+    let newPerson2 = {name: "Rodolfo", telephone: "910011001", age: 20};
+    
+    
+    let add_person  = new Promise ((resolve, reject) => {
+        let result = data.filter (person => person.telephone != newPerson.telephone);
+
+        if (result.length > 0)
+        
+        resolve (result);
+        
+        
+        else
+        
+        reject ("exist in vector");
+        
+        });
+        
+        add_person.then(result => {
+        // If we are here the promise has been correctly processed
+        
+        console.log("added:");
+        data.push(newPerson);
+        console.log(data);
+
+        }).catch(error => {
+        // if we are here there was an error
+        
+        console.log("Error:", error);
+        })  
+    
+
--- din/Block_2/JavaScript_practice/ej_arrays.js
+++ din/Block_2/JavaScript_practice/ej_arrays.js
@@ -1,9 +1,11 @@
-let data = [
+/*let data = [
     {name: "Nacho", telephone: "966112233", age: 40},
     {name: "Ana", telephone: "911223344", age: 35},
     {name: "Mario", phone: "611998877", age: 15},
     {name: "Laura", telephone: "633663366", age: 17}
     ];
+
+let data_older_30 ;
 
 console.log(data);
 
@@ -25,14 +27,40 @@ console.log(data);
 console.log("  Ordered by name" )
 data.sort(function(a, b) {
     
-    if (a.name < b.name) {
-        return -1;
-    }
-
-    if (a.name > b.name) {
-      return 1;
-    }
-   
+    return a.name.localeCompare(b.name)
+      
 })
 console.log(data);
 
+
+*/
+
+let data = [
+    {name: "Nacho", telephone: "966112233", age: 40},
+    {name: "Ana", telephone: "911223344", age: 35},
+    {name: "Mario", phone: "611998877", age: 15},
+    {name: "Laura", telephone: "633663366", age: 17}
+    ];
+    let OlderofAgePromise = new Promise ((resolve, reject) => {
+    let result = data.filter (person => person.age>= 18);
+    if (result.length> 0)
+    
+    resolve (result);
+    
+    
+    else
+    
+    reject ("No results");
+    
+    });
+    
+    OlderofAgePromise.then(result => {
+    // If we are here the promise has been correctly processed
+    
+    console.log("Matched coincidences:");
+    console.log(result);
+    }).catch(error => {
+    // if we are here there was an error
+    
+    console.log("Error:", error);
+    });
