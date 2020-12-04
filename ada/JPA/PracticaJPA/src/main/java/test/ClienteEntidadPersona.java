eef1e82b44d7d316c954308156b0b2180b6a4ab5
amrsin <amrsin@alu.iesserpis.org>
2020-12-04 10:45:22 +0100

    updated

---
+++ ada/JPA/PracticaJPA/src/main/java/test/ClienteEntidadPersona.java
@@ -0,0 +1,48 @@
+
+package test;
+import javax.persistence.*;
+import mx.com.gm.sga.domain.Persona;
+import org.apache.logging.log4j.*;
+/**
+ *
+ * @author amrsin
+ */
+public class ClienteEntidadPersona {
+    
+    static Logger log = LogManager.getRootLogger();
+    
+    public static void main(String[] args) {
+        
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("PersonaPU");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        
+        
+        //Empezamos la transacción
+        
+        tx.begin();
+        //nuevo objeto que vamos a insertar en la tala persona no ponemos id porque es AI
+        Persona p = new Persona("Maria", "Pérez", "Maria@gmail.com", 20);
+        log.debug("Objeto a persistir es " + p);
+        
+        //persistimos el objeto
+        em.persist(p);
+   
+        //teminamos la transacción
+        tx.commit();
+        log.debug("Objeto persistido: " + p);
+            
+        
+        
+        //cerramos el objeto EntityManeger
+        
+    
+
+        em.close();
+        
+        
+        
+        
+                
+    }   
+}
