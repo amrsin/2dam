9961f0afd4ed64250aa8ccd5069aa294a03cd23e
amrsin <amrsin@alu.iesserpis.org>
2020-12-07 11:08:03 +0100

    updated

--- ada/JPA/PracticaJPA/src/main/java/test/ClienteEntidadPersona.java
+++ ada/JPA/PracticaJPA/src/main/java/test/ClienteEntidadPersona.java
@@ -17,32 +17,26 @@ public class ClienteEntidadPersona {
         EntityManager em = emf.createEntityManager();
         EntityTransaction tx = em.getTransaction();
         
-        
         //Empezamos la transacción
-        
         tx.begin();
         //nuevo objeto que vamos a insertar en la tala persona no ponemos id porque es AI
         Persona p = new Persona("Maria", "Pérez", "Maria@gmail.com", 20);
         log.debug("Objeto a persistir es " + p);
-        
         //persistimos el objeto
         em.persist(p);
-   
         //teminamos la transacción
         tx.commit();
         log.debug("Objeto persistido: " + p);
-            
-        
-        
         //cerramos el objeto EntityManeger
-        
-    
-
         em.close();
         
-        
-        
-        
-                
+        em = emf.createEntityManager();
+        tx = em.getTransaction();
+        tx.begin();
+        p.setNombre("Maria Pepa");
+        p.setEmail("maria_perez@gmail.com");
+        em.merge(p);
+        tx.commit();
+        em.close();         
     }   
 }
