feb3dfb7e62d3ed87547ef40d1d4b14213967dfb
amrsin <amrsin@alu.iesserpis.org>
2020-12-16 12:35:19 +0100

    updated

--- din/Block_2/electron/hello_world/main.js
+++ din/Block_2/electron/hello_world/main.js
@@ -1,1 +1,28 @@
-console.log("main process working")
+
+const electron=require('electron');
+const app=electron.app;
+const BrowserWindow=electron.BrowserWindow;
+
+const path = require('path');
+const url = require('url');
+
+function createWindow() {
+let win=new BrowserWindow( {
+webPreferences:{
+nodeIntegration:true
+}
+}
+);
+win.loadURL(url.format({
+pathname:path.join(__dirname,'index.html'),
+protocol:'file',
+slashes:true
+}));
+win.webContents.openDevTools();
+win.on('closed',() =>{
+win=null;
+})
+}
+app.on('ready',createWindow);
+
+    
