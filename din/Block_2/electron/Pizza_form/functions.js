115fb02ce8f3cd276ff024aeb70f67e286bbfbf0
amrsin <amrsin@alu.iesserpis.org>
2021-01-12 15:05:24 +0100

    updated

---
+++ din/Block_2/electron/Pizza_form/functions.js
@@ -0,0 +1,19 @@
+let input_name = document.getElementById("input_name");
+let input_surname = document.getElementById("input_surname");
+let stick = document.getElementById("stick")
+let radio_vegetables = document.getElementById("radio_vegetables");
+let radio_classic = document.getElementById("radio_classic");
+let chk_cheese = document.getElementById("chk_cheese");
+let chk_spinish = document.getElementById("chk_spinish");
+let chk_peperoni = document.getElementById("chk_peperoni");
+
+let btn_ok = document.getElementById("btn_ok");
+
+btn_ok.addEventListener('click', () => {
+
+    if (input_name.value != "" && input_surname != "") {
+
+        if (radio_vegetables)
+        alert(input_name.value + " " + input_surname.value + " you odered " + )
+    }
+})
