caeaedbca2d1795c1a5b647af5258254ca3861cf
amrsin <amrsin@alu.iesserpis.org>
2021-01-19 12:01:34 +0100

    updated

--- PSP/ev2/ej_inetAddress/ej_inetAddress.java
+++ PSP/ev2/ej_inetAddress/ej_inetAddress.java
@@ -11,23 +11,38 @@ public static void main(String[] args) {
 	
 		
 		try {
-		InetAddress[] addresses = new InetAddress[2];
+			
+		InetAddress[] addresses = new InetAddress[4];
 		addresses[0] = InetAddress.getLoopbackAddress();
-		addresses[1] = InetAddress.getByName("ioc.xtec.cat");
-		addresses[2] = InetAddress.getByName("ioc.xtec.cat");
-		addresses[1] = InetAddress.getByName("224.0.0.15");
+		addresses[1] = InetAddress.getByName("iesserpis.org");
+		addresses[2] = InetAddress.getByName("192.168.4.18");
+	    addresses[3] = InetAddress.getByName("224.243.12.3");
+
 		
 			
 		for(InetAddress adress: addresses){
 			
+			System.out.println("Nombre: " + adress.getHostName());
+			System.out.println("IP: " + adress.getHostName());
+			
+			
 			if(adress.isLoopbackAddress()){
 				
-				System.out.println(adress.getHostName()+"té una adreça loopback");
+				System.out.println("Esta dirección es una dirección loopback");
+			}else if (adress.isSiteLocalAddress()){
+				
+				System.out.println("Esta dirección es un dirección local");
+		    }else if (adress.isMulticastAddress()) {
+				
+			     System.out.println("Esta dirección es un dirección multicast");
+
+				
 			}else{
 				
-				System.out.println(adress.getHostName() + " no té una adreça loopback");
-		}
-	}
+		       System.out.println("Esta dirección es un dirección privada");
+			}
+			System.out.println("");
+	    }
 	}
 		catch(Exception e) {
 	}
