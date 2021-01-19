679cd6ce2a4951abb41b680b37936b1c459bbb4e
amrsin <amrsin@alu.iesserpis.org>
2021-01-19 14:42:35 +0100

    updated

---
+++ din/Block_2/electron/first_CRUD/funtions.js
@@ -0,0 +1,52 @@
+
+const fs = require('fs');
+
+let btn_first = document.getElementById('btn_first');
+let btn_back = document.getElementById('btn_back');
+let btn_forward = document.getElementById('btn_forward');
+let btn_last = document.getElementById('btn_last');
+let btn_dlt = document.getElementById('btn_dlt');
+let btn_insert = document.getElementById('btn_insert');
+
+let clients = new Array();
+let pos = 0;
+let file = fs.readFileSync('./customers.json');
+
+clients = JSON.parse(file);
+
+let showCliente = () => {
+
+    document.getElementById('DNI').value = clients[pos].dni;
+    document.getElementById('Name').value = clients[pos].Name;
+    document.getElementById('Telefone').value = clients[pos].telephone;
+
+}
+
+btn_first.addEventListener('click', () => {
+
+
+    pos = 0;
+    showCliente();
+});
+
+btn_back.addEventListener('click', () => {
+
+
+    if (position > 0) {
+        position--;
+    }
+    showCliente();
+});
+
+
+btn_forward.addEventListener('click', () => {
+
+
+    pos ++
+    if (pos == clients.longitud()) {
+        pos--;
+    }
+    showCliente();
+
+});
+
---
+++ din/Block_2/electron/first_CRUD/main.js
@@ -0,0 +1,17 @@
+// THE usual one we use:
+const { app, BrowserWindow } = require('electron')
+function createWindow() {
+    let mainWindow = new BrowserWindow({
+        width: 1000,
+        height:700,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+    // remove default chromium menu
+    mainWindow.setMenu(null);
+    mainWindow.loadFile('index.html')
+    // Open the DevTools.
+     mainWindow.webContents.openDevTools ()
+}
+app.whenReady().then(createWindow);
