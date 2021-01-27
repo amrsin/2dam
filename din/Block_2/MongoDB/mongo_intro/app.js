36759fd6d6477b3600276cde810feb58e65fbb70
amrsin <amrsin@alu.iesserpis.org>
2021-01-27 18:11:42 +0100

    updated

--- din/Block_2/MongoDB/mongo_intro/app.js
+++ din/Block_2/MongoDB/mongo_intro/app.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 // this is necessary because mongo supports different types of promises
 mongoose.Promise = global.Promise;
 // connect with the contacts DB
-const url = 'mongodb+srv://root:root@cluster0.m3met.mongodb.net/contacts?retryWrites=true&w=majority';
+const url = 'mongodb+srv://root:root@cluster0.m3met.mongodb.net/Contacts?retryWrites=true&w=majority';
 mongoose.connect(url, {
     useNewUrlParser: true,
     useCreateIndex: true,
@@ -47,7 +47,7 @@ contactSchema = new mongoose.Schema({
         max: 120
     }
 });
-
+/*
 // add documents
 // first we create the contact
 let contact1 = new Contact({
@@ -68,7 +68,6 @@ let p1 = contact1.save().then(result => {
 Promise.all([p1]).then(values => {
     mongoose.connection.close();
 });
-
 
 // search with find
 let p2 = Contact.find().then(result => {
@@ -93,7 +92,6 @@ let p3 = Contact.find({ name: 'Boris', age: 49 }).then(result => {
 Promise.all([p1, p2, p3]).then(values => {
     mongoose.connection.close();
 });
-
 // delete documents
 let p4 = Contact.remove({ name: 'Boris' }).then(result => {
     console.log(result);
@@ -105,21 +103,14 @@ let p4 = Contact.remove({ name: 'Boris' }).then(result => {
 Promise.all([p1, p2, p3, p4]).then(values => {
     mongoose.connection.close();
 });
-
-
-// Update documents
-let p5=Contact.findByIdAndUpdate('5ede78b4c5e89d072c3d1a71',
-      {name: 'Boris' },
-      {name: 'Anaya Moreno' ,age:51}, 
-      {new:true})
-.then(result=>{
-console.log("Modified contact:",result);
-}).catch(error=>{
-console.log("ERROR:",error);
-});
-// we must wait for all promises to finish
-// to close the connection to the database
-Promise.all([p1, p2, p3, p4, p5]).then(values => {
-    mongoose.connection.close();
-});
 */
+let p5 = Contact.findByIdAndUpdate('60119b8b37e67032a4a05ad0',
+    {name: 'Boris'},  {name: 'Anaya Moreno', age: 51}, { new: true })
+    .then(result => {
+        console.log("Modified contact:", result);
+    }).catch(error => {
+        console.log("ERROR:", error);
+    });
+    Promise.all(p5).then(values=>{
+        mongoose.connection.close();
+        });
