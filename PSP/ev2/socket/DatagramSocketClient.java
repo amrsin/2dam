66f162d307917553f5977337528cb6d5e09c9cb4
amrsin <amrsin@alu.iesserpis.org>
2021-02-02 19:34:52 +0100

    updated

--- PSP/ev2/socket/DatagramSocketClient.java
+++ PSP/ev2/socket/DatagramSocketClient.java
@@ -8,43 +8,45 @@ public class DatagramSocketClient {
 	int serverPort;
 	DatagramSocket socket;
 
-public void init(String host, int port) throws SocketException,
-	UnknownHostException{
+	public void init(String host, int port) throws SocketException, UnknownHostException {
 		serverIP = InetAddress.getByName(host);
 		serverPort = port;
 		socket = new DatagramSocket();
-}
-public void runClient() throws IOException{
-	byte [] receivedData = new byte[1024];
-	byte [] sendingData;
-	//a l’inici
-	sendingData = getFirstRequest();
-	//el servidor atén el port indefinidament
-	while(mustContinue(sendingData)){
-		DatagramPacket packet = new DatagramPacket(sendingData,
-		sendingData.length, serverIP, serverPort);
-		//enviament de la resposta
-		socket.send(packet);
-		//creació del paquet per rebre les dades
-		packet = new DatagramPacket(receivedData, 1024);
-		//espera de les dades
-		socket.receive(packet);
-		//processament de les dades rebudes i obtenció de la resposta
-		sendingData = getDataToRequest(packet.getData(), packet.getLength());
 	}
-}
-private byte[] getDataToRequest(byte[] data, int length) {
-	//procés diferent per cada aplicació
-	return null;
- }
-private byte[] getFirstRequest() {
-	//procés diferent per cada aplicació
-	return null;
 
- }
-private boolean mustContinue(byte[] sendingData) {
-	//procés diferent per cada aplicació
-    return true;
+	public void runClient() throws IOException {
+		byte[] receivedData = new byte[1024];
+		byte[] sendingData;
+		// a lâ€™inici
+		sendingData = getFirstRequest();
+		// el servidor atÃ©n el port indefinidament
+		while (mustContinue(sendingData)) {
+			DatagramPacket packet = new DatagramPacket(sendingData, sendingData.length, serverIP, serverPort);
+			// enviament de la resposta
+			socket.send(packet);
+			// creaciÃ³ del paquet per rebre les dades
+			packet = new DatagramPacket(receivedData, 1024);
+			// espera de les dades
+			socket.receive(packet);
+			// processament de les dades rebudes i obtenciÃ³ de la resposta
+			sendingData = getDataToRequest(packet.getData(), packet.getLength());
+		}
+	}
 
-  }
+	private byte[] getDataToRequest(byte[] data, int length) {
+		// procÃ©s diferent per cada aplicaciÃ³
+		return null;
+	}
+
+	private byte[] getFirstRequest() {
+		// procÃ©s diferent per cada aplicaciÃ³
+		return null;
+
+	}
+
+	private boolean mustContinue(byte[] sendingData) {
+		// procÃ©s diferent per cada aplicaciÃ³
+		return true;
+
+	}
 }
--- PSP/ev2/socket/DatagramSocketServer.java
+++ PSP/ev2/socket/DatagramSocketServer.java
@@ -1,4 +1,5 @@
 
+
 import java.util.*;
 import java.io.*;
 import java.net.*;
@@ -6,77 +7,70 @@ import java.net.*;
 public class DatagramSocketServer {
 	DatagramSocket socket;
 
-public void init(int port) throws SocketException{
-	socket = new DatagramSocket(port);
-}
-
-public void runServer() throws IOException{
-	
-	byte [] receivingData = new byte[1024];
-	byte [] sendingData;
-	InetAddress clientIP;
-	int clientPort;
-	//el servidor atén el port indefinidament
-	while(true){
-	//creació del paquet per rebre les dades
-	DatagramPacket packet = new DatagramPacket(receivingData, 1024);
-	//espera de les dades
-	socket.receive(packet);
-	
-	traducir(packet.getData(), packet.getLength());
-	//processament de les dades rebudes i obtenció de la resposta
-	sendingData = processData(packet.getData(), packet.getLength());
-	//obtenció de l’adreça del client
-	clientIP = packet.getAddress();
-	//obtenció del port del client
-	clientPort = packet.getPort();
-	//creació del paquet per enviar la resposta
-	packet = new DatagramPacket(sendingData, sendingData.length,
-
-	clientIP, clientPort);
-
-	//enviament de la resposta
-	socket.send(packet);
+	public void init(int port) throws SocketException {
+		socket = new DatagramSocket(port);
 	}
- }
+
+	public void runServer() throws IOException {
+
+		byte[] receivingData = new byte[1024];
+		byte[] sendingData;
+		InetAddress clientIP;
+		int clientPort;
+		// el servidor atÃ©n el port indefinidament
+		while (true) {
+			// creaciÃ³ del paquet per rebre les dades
+			DatagramPacket packet = new DatagramPacket(receivingData, 1024);
+			// espera de les dades
+			socket.receive(packet);
+
+			String data_traducido = traducir(packet.getData(), packet.getLength());
+			// processament de les dades rebudes i obtenciÃ³ de la resposta
+			String msg = processData(data_traducido);
+			sendingData = msg.getBytes();
+			// obtenciÃ³ de lâ€™adreÃ§a del client
+			clientIP = packet.getAddress();
+			// obtenciÃ³ del port del client
+			clientPort = packet.getPort();
+			// creaciÃ³ del paquet per enviar la rezsposta
+			packet = new DatagramPacket(sendingData, sendingData.length,
+
+					clientIP, clientPort);
+
+			// enviament de la resposta
+			socket.send(packet);
+		}
+	}
+
 	private String processData(String data_traducido) {
-	//procés diferent per cada aplicació
-	
+		// procÃ©s diferent per cada aplicaciÃ³
+
+		String data = "";
 		switch (data_traducido) {
 
- 
+		case "noticias":
 
-        case "noticias":
+			data = "Esto es una noticia de prueba";
+			break;
 
+		case "hora":
 
-        break;
+			break;
 
+		default:
 
-        case valor2:
+			break;
 
+		}
+		return data;
 
-        break;
-
-  
-
-        default:
-
-		
-        break;
-	
-	
-	
-	return null;
 	}
-	
-	public static String traducir(byte[] data, int length) {
-				
-		
 
- }
-				
-		
-	return null;
-		
-    }
+	public static String traducir(byte[] data, int length) {
+
+		String data_traducido = new String(data, 0, length);
+
+		return data_traducido;
+
+	}
 }
