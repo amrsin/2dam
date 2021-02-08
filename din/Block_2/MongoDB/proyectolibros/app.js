55c8ad6a9a2fc3db75b9e8fe3bc67da5d92c8872
amrsin <amrsin@alu.iesserpis.org>
2021-02-08 13:44:37 +0100

    updated

--- din/Block_2/MongoDB/proyectolibros/app.js
+++ din/Block_2/MongoDB/proyectolibros/app.js
@@ -1,6 +1,9 @@
 
 const mongoose = require('mongoose');
 let btn_buscar = document.getElementById("btn_buscar");
+let btn_author = document.getElementById("btn_author");
+let btn_home = document.getElementById("btn_home");
+let btn_save = document.getElementById("btn_save");
 
 mongoose.Promise = global.Promise;
 //conexión
@@ -53,8 +56,8 @@ let p1 = Libro.find().then(resultado => {
 });
 
 btn_buscar.addEventListener('click', () => {
-    let txt_buscar = document.getElementById('txt_buscar').nodeValue;
-    Libro.find({title: {$regex:}}).then(resultado => {
+    let txt_buscar = document.getElementById('txt_buscar').value;
+    Libro.find({ title: { $regex: '.*' + txt_buscar + '.*' } }).then(resultado => {
         let cadenaDOM = "";
         resultado.forEach(book => {
             cadenaDOM +=
@@ -66,10 +69,74 @@ btn_buscar.addEventListener('click', () => {
                     </x-box>
                 </div>`;
         });
+        document.getElementById("wrapper").innerHTML = cadenaDOM;
+    }).catch(error => {
+        console.log("Error en find");
+    });
+});
 
+btn_author.addEventListener('click', () => {
+    let txt_author = document.getElementById('txt_author').value;
+    Libro.find({ author: { $regex: '.*' + txt_author + '.*' } }).then(resultado => {
+        let cadenaDOM = "";
+        resultado.forEach(book => {
+            cadenaDOM +=
+                `<div>
+                    <x-box vertical>
+                        <img src="./images/${book.img}" height="170" width="108">
+                        <x-label><strong>${book.title}</strong></x-label>
+                        <x-label>${book.author}</x-label>
+                    </x-box>
+                </div>`;
+        });
+        document.getElementById("wrapper").innerHTML = cadenaDOM;
+    }).catch(error => {
+        console.log("Error en find");
+    });
+});
+
+btn_home.addEventListener('click', () => {
+
+    let p1 = Libro.find().then(resultado => {
+        let cadenaDOM = "";
+        resultado.forEach(book => {
+            cadenaDOM +=
+                `<div>
+                <x-box vertical>
+                    <img src="./images/${book.img}" height="170" width="108">
+                    <x-label><strong>${book.title}</strong></x-label>
+                    <x-label>${book.author}</x-label>
+                </x-box>
+            </div>`;
+        });
+        document.getElementById("wrapper").innerHTML = cadenaDOM;
+    }).catch(error => {
+        console.log("ERROR en find");
+    });
+});
+
+btn_save.addEventListener('click', () => {
+
+  
+
+    let txt_title = document.getElementById('txt_title').value;
+    let txt_author_new = document.getElementById('txt_author_new').value;
+    let txt_img = document.getElementById('txt_img').value;
+    
+    let new_book = new Libro({
+        title: txt_title,
+        author: txt_author_new,
+        img: txt_img
+    });
+
+    let p = new_book.save().then(result => {
+        console.log("book added:", result);
+    }).catch(error => {
+        console.log("ERROR adding book:", error);
+    });
 
 });
-  
+
 
 /*const inicio = (books => {
     let cadenaDOM = "";
