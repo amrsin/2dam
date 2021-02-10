b647c1306e0384da2c9a5f4e963d85a1c0f99f28
amrsin <amrsin@alu.iesserpis.org>
2021-02-10 19:18:34 +0100

    updated

--- din/Block_2/MongoDB/Auto_dealer/app.js
+++ din/Block_2/MongoDB/Auto_dealer/app.js
@@ -4,11 +4,20 @@ let btn_save = document.getElementById('btn_save');
 let btn_dlt = document.getElementById('btn_dlt');
 let btn_search = document.getElementById('btn_search');
 let agree_button = document.getElementById('agree_button');
-
-
 let txt_license_plate_dlt = document.getElementById('txt_license_plate_dlt');
-
-
+let txt_license_plate_update = document.getElementById('txt_license_plate_update')
+let txt_model_update = document.getElementById('txt_model_update')
+let txt_brand_update = document.getElementById('txt_brand_update')
+let txt_year_update = document.getElementById('txt_year_update')
+let txt_color_update = document.getElementById('txt_color_update')
+let txt_kilometres_update = document.getElementById('txt_kilometres_update')
+let txt_fuel_update = document.getElementById('txt_fuel_update')
+let txt_doors_update = document.getElementById('txt_doors_update')
+let txt_gear_change_update = document.getElementById('txt_gear_change_update')
+let txt_seats_update = document.getElementById('txt_seats_update')
+let txt_price_update = document.getElementById('txt_price_update')
+let notification = document.querySelector("#notification");
+            
 mongoose.Promise = global.Promise;
 //conexión
 mongoose.connect('mongodb://localhost:27017/auto_dealer', {
@@ -121,10 +130,14 @@ btn_save.addEventListener('click', () => {
     });
 
     let c = new_car.save().then(result => {
-        alert("Car added")
+        notification.innerHTML = "Car added";
+        notification.opened = true;
+        console.log('Car added');
         find();
     }).catch(error => {
-        alert("Error adding car")
+        notification.innerHTML = "Error adding add";
+        notification.opened = true;
+        console.log("Error adding car", error)
 
     });
 });
@@ -135,23 +148,29 @@ btn_dlt.addEventListener('click', e => {
 
         if(result.length!=0) {
             c_model.remove({ license_plate: txt_license_plate_dlt.value }).then(result => {
-                alert("Car deleted")
+                notification.innerHTML = "Car deleted";
+                notification.opened = true;
+                console.log("Car deleted")
                 find()
             }).catch(error => {
-                alert('Error deleting car')
+                notification.innerHTML = "Error deleting car";
+                notification.opened = true;
+                console.log('Error deleting car')
             });
         }else {
-            alert("Car with license plate " + txt_license_plate_dlt.value + " doesn't exist in bd");
+            notification.innerHTML = "Car with license plate " + txt_license_plate_dlt.value + " doesn't exist in bd";
+            notification.opened = true;
+            console.log("Car with license plate " + txt_license_plate_dlt.value + " doesn't exist in bd");
         }
         
     }).catch(error => {
-        console.log("ERROR:", error);
+        notification.innerHTML = "Error in delete";
+        notification.opened = true;
+        console.log("ERROR in delete", error);
     });
    
 });
 
-let txt_license_plate_update = document.getElementById('txt_license_plate_update')
-let txt_model_update = document.getElementById('txt_model_update')
 btn_search.addEventListener('click', e => {
     
     
@@ -159,19 +178,29 @@ btn_search.addEventListener('click', e => {
 
         if(result.length!=0) {
 
+            result.forEach(car =>{
+                 
+                txt_model_update.value = `${car.model}`;
+                txt_brand_update.value = `${car.brand}`;
+                txt_year_update.value = `${car.year}`;
+                txt_color_update.value = `${car.color}`;
+                txt_kilometres_update.value = `${car.kilometres}`;
+                txt_fuel_update.value = `${car.fuel}`;
+                txt_doors_update.value = `${car.doors}`;
+                txt_gear_change_update.value = `${car.gear_change}`;
+                txt_seats_update.value = `${car.seats}`;
+                txt_price_update.value = `${car.price}`;
+                
+            }); 
 
-            
         }else {
             alert("Car with license plate " + txt_license_plate_update.value + " doesn't exist in bd");
         }
         
     }).catch(error => {
-        console.log("ERROR:", error);
-    });
-   
+        alert("ERROR in search");
+    });  
 });
-
-
 
 agree_button.addEventListener('click', e => {
 
@@ -247,6 +276,8 @@ function find() {
             })*/
         });
     }).catch(error => {
-        console.log("ERROR en find");
+        notification.innerHTML = "Error in find";
+        notification.opened = true;
+        console.log("ERROR in find");
     });
 }
